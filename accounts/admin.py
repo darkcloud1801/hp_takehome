fc6c08f9918a1a0c22d0c6d793377cf4ed1930fc
Rubin Damian <rubin@klicktrack.io>
2024-09-09 14:00:17 -0500

    Admin
    Added a Snippet Migration
    Added fixtures for account users
    Added fixtures for snippets
    Added migration files
    Added model for audit log
    Added functionality to save audit log
    Added endpoint for CRUD for users
    Added tests for user, snippets, and audit logs

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,35 @@
+from django.apps import apps
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from .models import CustomUser
 
-admin.site.register(CustomUser, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+
+    def save_model(self, request, obj, form, change):
+        if change:
+            action = 'updated'
+        else:
+            action = 'created'
+
+        super().save_model(request, obj, form, change)
+
+        audit_log_model = apps.get_model("history", "AuditLog")
+        audit_log_model.objects.create(
+            action=action,
+            model_name=obj.__class__.__name__,
+            object_id=obj.pk,
+            user=request.user
+        )
+
+    def delete_model(self, request, obj):
+        audit_log_model = apps.get_model("history", "AuditLog")
+        audit_log_model.objects.create(
+            action='deleted',
+            model_name=obj.__class__.__name__,
+            object_id=obj.pk,
+            user=request.user,
+        )
+        super().delete_model(request, obj)
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- snippets/admin.py
+++ snippets/admin.py
@@ -1,3 +1,4 @@
+from django.apps import apps
 from django.contrib import admin
 
 from .models import Snippet
@@ -5,6 +6,32 @@ from .models import Snippet
 
 class SnippetAdmin(admin.ModelAdmin):
     readonly_fields = ("highlighted",)
+
+    def save_model(self, request, obj, form, change):
+        if change:
+            action = 'updated'
+        else:
+            action = 'created'
+
+        super().save_model(request, obj, form, change)
+
+        audit_log_model = apps.get_model("history", "AuditLog")
+        audit_log_model.objects.create(
+            action=action,
+            model_name=obj.__class__.__name__,
+            object_id=obj.pk,
+            user=request.user
+        )
+
+    def delete_model(self, request, obj):
+        audit_log_model = apps.get_model("history", "AuditLog")
+        audit_log_model.objects.create(
+            action='deleted',
+            model_name=obj.__class__.__name__,
+            object_id=obj.pk,
+            user=request.user,
+        )
+        super().delete_model(request, obj)
 
 
 admin.site.register(Snippet, SnippetAdmin)
